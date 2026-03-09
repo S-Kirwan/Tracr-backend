30d8b47b34ffd5c7421e5307bb879090820154fa
Sean Kirwan <seandarrakirwan@gmail.com>
2026-03-09 22:29:46 +0000

    Added run seed file

---
+++ db/seeds/run-seed.js
@@ -0,0 +1,11 @@
+import devData from "../data/development-data/index.js";
+import seed from "./seed.js";
+import db from "../connection.js";
+
+const runSeed = async () => {
+	await seed(devData);
+	db.end();
+	return;
+};
+
+runSeed();
