11eb0b94ccc89d419cdec350593e77a16f03a19b
Sean Kirwan <seandarrakirwan@gmail.com>
2026-03-09 14:35:23 +0000

    Added eslint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginNode from "eslint-plugin-n";
+
+export default [
+  js.configs.recommended,
+  pluginNode.configs["flat/recommended"],
+
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    
+    rules: {
+      "n/no-process-exit": "error",
+    },
+
+    ignores: ["node_modules/"],
+  },
+];
