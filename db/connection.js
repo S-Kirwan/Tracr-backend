3fcf54ae272459355cf6f99b586bb2e041cf8471
Sean Kirwan <seandarrakirwan@gmail.com>
2026-03-09 22:25:15 +0000

    Added database connection

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+const ENV = process.env.NODE_ENV || 'development'
+
+dotenv.config({path: `${__dirname}/../.env.${ENV}`})
+
+const	config = {};
+
+if (ENV === "production")
+{
+	config.connectionString = process.env.DATABASE_URL;
+	config.max = 2;
+}
+
+const db = new Pool(config);
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("No PGDATABASE configured")
+} else {
+    console.log(`Connected to ${process.env.PGDATABASE || process.env.DATABASE_URL}`)
+}
+
+export default db;
