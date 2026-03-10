87214b4451918ea700f3aa5dc90181e4fa490cb4
Sean Kirwan <seandarrakirwan@gmail.com>
2026-03-10 11:11:28 +0000

    Added Dummy users and shapes data

---
+++ db/data/index.js
@@ -0,0 +1,5 @@
+import runsData from "./runs.js";
+import shapesData from "./shapes.js";
+import usersData from "./users.js";
+
+export { runsData, shapesData, usersData };
---
+++ db/data/users.js
@@ -0,0 +1,22 @@
+const usersData = [
+	{
+		username: "big runner",
+		password: "RunningForLife22",
+		name: "Tyler Durden",
+	},
+	{
+		username: "picassoRunnr",
+		password: "guernica",
+		name: "Jack Smith",
+	},
+	{
+		username: "vanGogh",
+		password: "starry_night21",
+		name: "Vincent Van Gogh",
+	},
+	{
+		username: "Leonardo",
+		password: "TMNT_rocks",
+		name: "Leonardo Da Vinci",
+	},
+];
