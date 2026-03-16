220ba6884b798beee203b537e508a71d71a4ccba
Sean Kirwan <seandarrakirwan@gmail.com>
2026-03-16 10:14:11 +0000

    Imports bugfix
    Full path index.js for ESM

--- routes/expeditions.route.js
+++ routes/expeditions.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 
-import { expeditionsController } from "../controllers";
-import { invalidMethod } from "../middleware";
+import { expeditionsController } from "../controller/index.js";
+import { invalidMethod } from "../middleware/index.js";
 
 const router = express.Router();
 
 router
-    .route("/")
-    .get(expeditionsController.getAllExpeditions)
-    .all(invalidMethod);
+	.route("/")
+	.get(expeditionsController.getAllExpeditions)
+	.all(invalidMethod);
 
 export default router;
