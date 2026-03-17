0e4f48f7edcafe5d9ae097368e0a33e317322d07
Sean Kirwan <seandarrakirwan@gmail.com>
2026-03-17 11:27:00 +0000

    Added function to validate request queries on leaderboards

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import {
 	shapesRouter,
 	usersRouter,
 	expeditionsRouter,
+	leaderboardsRouter,
 } from "./routes/index.js";
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use("/api/shapes", shapesRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/expeditions", expeditionsRouter);
+app.use("/api/leaderboards", leaderboardsRouter);
 
 app.all("/*splat", urlNotFound);
 
